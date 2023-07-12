ed9c614cf635d956ab30be3f02c8de4e427783e0
Francisco Javier <fjsmdemairena@gmail.com>
2023-07-12 22:12:47 +0200

    ~ part2: Adds local server to fetch data
    It uses the db.json file as a local database, as well as json-server module to fetch its data.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("GET persons")
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
